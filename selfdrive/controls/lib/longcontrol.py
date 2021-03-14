24ba98501c86a4fea1e8af05621bc52b2ccbc5d7
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-03-14 23:19:34 +0800

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -8,12 +8,12 @@ from common.travis_checker import travis
 
 LongCtrlState = log.ControlsState.LongControlState
 
-STOPPING_EGO_SPEED = 0.5
+STOPPING_EGO_SPEED = 0.6
 STOPPING_TARGET_SPEED_OFFSET = 0.01
-STARTING_TARGET_SPEED = 0.8
+STARTING_TARGET_SPEED = 0.7
 BRAKE_THRESHOLD_TO_PID = 0.2
 
-BRAKE_STOPPING_TARGET = 0.8  # apply at least this amount of brake to maintain the vehicle stationary
+BRAKE_STOPPING_TARGET = 1.0  # apply at least this amount of brake to maintain the vehicle stationary
 
 RATE = 100.0
 
