ada8f2cc8cfbe1b459d0c9297bd8c16a7ef05c68
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-04-08 13:09:06 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -278,7 +278,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.outerLoopGainBP = [5.5, 8.3, 11.1, 13.9, 16.7, 19.4, 22.2, 25, 28.4, 30.3]
       ret.lateralTuning.indi.outerLoopGainV = [3.0, 4.63, 6.35, 8.1, 9.85, 11.7, 13.6, 15.4, 17.2, 19] # optimal centering values for curving; last 3 values for smoother ALC and solid lane centering
       ret.lateralTuning.indi.timeConstantBP = [5.5, 8.3, 11.1, 13.9, 16.7, 19.4, 22.2, 30.09, 30.1]
-      ret.lateralTuning.indi.timeConstantV = [0.3, 0.4, 0.6, 0.8, 1, 1.3, 3.0, 3.0, 6.0] # optimized course plotting for low speed curves; high last value produces smoother ALC and ride quality
+      ret.lateralTuning.indi.timeConstantV = [0.3, 0.41, 0.62, 0.83, 1, 1.3, 3.0, 3.0, 6.0] # optimized course plotting for low speed curves; high last value produces smoother ALC and ride quality
       ret.lateralTuning.indi.actuatorEffectivenessBP = [5.5, 8.3, 11.1, 13.9, 16.7, 19.4, 22.2, 25]
       ret.lateralTuning.indi.actuatorEffectivenessV = [17, 17, 17, 17, 17, 17, 17, 17] # balance between high angle curving and rocking
 
