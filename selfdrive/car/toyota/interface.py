abbbf0d594728e8459e4399d63d209a0f7ec7858
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-04-23 22:44:03 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -230,8 +230,8 @@ class CarInterface(CarInterfaceBase):
       ret.stoppingBrakeRate = 0.12 # reach stopping target smoothly
       ret.startingBrakeRate = 0.9 # release brakes fast
       ret.startAccel = 1.50 # Accelerate from 0 faster
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.65], [0.15]]
-      ret.lateralTuning.pid.kf = 0.000079
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.08]]
+      ret.lateralTuning.pid.kf = 0.00007818594
 
     elif candidate == CAR.RAV4H_TSS2:
       stop_and_go = True
