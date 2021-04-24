11778c00a7481c06741899607afd9f65ca5fa4d7
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-04-25 00:10:07 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -231,13 +231,14 @@ class CarInterface(CarInterfaceBase):
       ret.startingBrakeRate = 0.9 # release brakes fast
       ret.startAccel = 1.50 # Accelerate from 0 faster
       ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kiBP =  [0, 11, 17, 25, 40]
       ret.lateralTuning.pid.kpBP =  [0, 11, 17, 25, 40]
+      ret.lateralTuning.pid.kiBP =  [0, 11, 17, 25, 40]
       ret.lateralTuning.pid.kfBP = [0, 11, 17, 25, 40]
+      ret.lateralTuning.pid.kdBP = [0, 11, 17, 25, 40]
       ret.lateralTuning.pid.kpV = [0.6, 0.6, 0.6, 0.3, 0.2]
       ret.lateralTuning.pid.kiV = [0.1, 0.1, 0.1, 0.05, 0.02]
       ret.lateralTuning.pid.kfV = [0.00007818594, 0.00007818594, 0.00007818594, 0.00006, 0.00004]
-      ret.lateralTuning.pid.kdV = [2.0]
+      ret.lateralTuning.pid.kdV = [2.0, 2.0, 2.0, 2.0, 2.0]
       ret.lateralTuning.pid.newKfTuned = False
 
     elif candidate == CAR.RAV4H_TSS2:
