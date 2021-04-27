31531b2684b5a408b671012db73dbfc9fc61a107
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-04-27 19:31:47 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -246,7 +246,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kpBP = [0, 11, 16, 18, 22, 28, 33]
         ret.lateralTuning.pid.kiBP = [0, 11, 16, 18, 22, 28, 33]
         ret.lateralTuning.pid.kfBP = [0, 11, 16, 18, 22, 28, 33]
-        ret.lateralTuning.pid.kpV = [0.485, 0.45, 0.45, 0.45, 0.45, 0.3, 0.15]
+        ret.lateralTuning.pid.kpV = [0.45, 0.4, 0.4, 0.4, 0.35, 0.3, 0.15]
         ret.lateralTuning.pid.kiV = [0.1, 0.1, 0.08, 0.08, 0.05, 0.003, 0.002]
         ret.lateralTuning.pid.kfV = [0.000153263811757641, 0.000153263811757641, 0.000153263811757641, 0.00007818594, 0.000062548752, 0.000054730158, 0.00004]
         #ret.lateralTuning.pid.newKfTuned = True   0.000153263811757641
