5b7497aabb707094a77cb15684b3f176a66906a3
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-04-28 13:16:45 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -252,7 +252,7 @@ class CarInterface(CarInterfaceBase):
             ret.lateralTuning.pid.kfBP = [0, 8, 16, 22, 28, 33, 40]
             ret.lateralTuning.pid.kpV = [0.46, 0.45, 0.44, 0.43, 0.3, 0.15, 0.15]
             ret.lateralTuning.pid.kiV = [0.12, 0.1, 0.08, 0.06, 0.05, 0.03, 0.02]
-            ret.lateralTuning.pid.kfV = [0.00012823785, 0.00011155388, 0.00009486991, 0.00007818594, 0.000062548752, 0.000054730158, 0.00004]
+            ret.lateralTuning.pid.kfV = [0.00013823785, 0.00011822054, 0.00009820324, 0.00007818594, 0.000062548752, 0.000054730158, 0.00004]
             break
 
     elif candidate == CAR.RAV4H_TSS2:
