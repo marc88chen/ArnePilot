b20ff4eb571ad7760b908002e694b240e748af97
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-04-29 20:31:54 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -226,7 +226,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
       ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
       ret.longitudinalTuning.kiV = [.32, .21, .20, .17, .1]
-      ret.stoppingBrakeRate = 0.14 # reach stopping target smoothly
+      ret.stoppingBrakeRate = 0.13 # reach stopping target smoothly
       ret.startingBrakeRate = 0.85 # release brakes fast
       ret.startAccel = 1.50 # Accelerate from 0 faster
       if spairrowtuning:
@@ -266,9 +266,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
       ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
       ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
-      ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
-      ret.stoppingBrakeRate = 0.14 # reach stopping target smoothly
-      ret.startingBrakeRate = 1.21 # release brakes fast
+      ret.longitudinalTuning.kiV = [.32, .21, .20, .17, .1]
+      ret.stoppingBrakeRate = 0.13 # reach stopping target smoothly
+      ret.startingBrakeRate = 0.85 # release brakes fast
       ret.startAccel = 1.50 # Accelerate from 0 faster
       if spairrowtuning:
         ret.steerRateCost = 1.0
@@ -288,11 +288,11 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kfV = [0.00004]
         for fw in car_fw:
           if fw.ecu == "eps" and fw.fwVersion == b"8965B42170\x00\x00\x00\x00\x00\x00":
-            ret.lateralTuning.pid.kpBP = [0, 8, 16, 22, 28, 33, 40]
-            ret.lateralTuning.pid.kiBP = [0, 8, 16, 22, 28, 33, 40]
-            ret.lateralTuning.pid.kfBP = [0, 8, 16, 22, 28, 33, 40]
+            ret.lateralTuning.pid.kpBP = [0, 8, 16, 23.6, 28, 33, 40]
+            ret.lateralTuning.pid.kiBP = [0, 8, 16, 23.6, 28, 33, 40]
+            ret.lateralTuning.pid.kfBP = [0, 8, 16, 23.6, 28, 33, 40]
             ret.lateralTuning.pid.kpV = [0.46, 0.45, 0.44, 0.43, 0.3, 0.15, 0.15]
-            ret.lateralTuning.pid.kiV = [0.12, 0.1, 0.08, 0.06, 0.05, 0.03, 0.02]
+            ret.lateralTuning.pid.kiV = [0.12, 0.1, 0.1, 0.1, 0.05, 0.03, 0.02]
             ret.lateralTuning.pid.kfV = [0.000153263811757641, 0.00013823785, 0.00012822054, 0.00010820324, 0.00007818594, 0.000062548752, 0.00004]
             break
 
