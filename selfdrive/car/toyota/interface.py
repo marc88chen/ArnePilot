a2b6728a6ff0263bf8afdfc19093fe86a462a62d
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-05-16 20:46:10 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -251,7 +251,7 @@ class CarInterface(CarInterfaceBase):
             ret.lateralTuning.pid.kiBP = [8, 23, 23.01, 32]
             ret.lateralTuning.pid.kfBP = [0, 22, 40]
             ret.lateralTuning.pid.kpV = [0.6, 0.6, 0.2, 0.2] # power or torque
-            ret.lateralTuning.pid.kiV = [0.516, 0.516, 0.025, 0.025] # trajectory variance
+            ret.lateralTuning.pid.kiV = [0.512, 0.514, 0.025, 0.025] # trajectory variance
             ret.lateralTuning.pid.kfV = [0.0000785398163397448, 0.0000785398163397448, 0.00004] # curvature
             #ret.lateralTuning.pid.kpBP = [0, 23.6, 23.61, 40]
             #ret.lateralTuning.pid.kiBP = [0, 23.6, 23.61, 40]
