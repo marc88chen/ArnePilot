98504ca0e36ea0d64e0d237620892e5afdb62add
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-06-10 17:12:10 +0800

    Update __init__.py

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -249,7 +249,7 @@ class DynamicFollow:
       df_profile = self.dp_dynamic_follow
 
     if df_profile == PROFILE_LONG:
-      y_dist = [1.33, 1.38, 1.44, 1.56, 1.679, 1.74, 1.792, 1.833, 1.876, 1.92, 2.03, 2.1, 2.19, 2.3, 2.43, 2.6]
+      y_dist = [1.33, 1.35, 1.42, 1.56, 1.679, 1.74, 1.792, 1.833, 1.876, 1.92, 2.03, 2.1, 2.19, 2.3, 2.43, 2.6]
       profile_mod_pos = [0.8, 0.7, 0.4, 0.1, 0.088]
       profile_mod_neg = [0.8, 0.6, 1.0, 1.1, 1.2]
     elif df_profile == PROFILE_SHORT:  # for in congested traffic
