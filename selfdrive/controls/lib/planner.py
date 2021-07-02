069117fceb1c41b98853c25c168c4c0949f90e19
cgw1968-5779 <58063525+cgw1968-5779@users.noreply.github.com>
2021-07-03 00:36:19 +0800

    Update planner.py

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -62,10 +62,10 @@ _DP_CRUISE_MIN_V_SPORT = [-3.0, -2.6, -2.3, -2.0, -1.0]
 _DP_CRUISE_MIN_V_FOLLOWING = [-3.0, -2.5, -2.0, -1.5, -1.0]
 _DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 55.0]
 
-_DP_CRUISE_MAX_V = [2.0, 2.0, 1.5, .5, .3]
-_DP_CRUISE_MAX_V_ECO = [0.8, 0.9, 1.0, 0.4, 0.2]
-_DP_CRUISE_MAX_V_SPORT = [3.0, 3.5, 3.0, 2.0, 2.0]
-_DP_CRUISE_MAX_V_FOLLOWING = [1.6, 1.4, 1.4, .7, .3]
+_DP_CRUISE_MAX_V = [2.5, 1.6, 1.3, .65, .2]
+_DP_CRUISE_MAX_V_ECO = [2.0, 1.3, 0.85, 0.4, 0.11]
+_DP_CRUISE_MAX_V_SPORT = [3.5, 3.0, 2.5, 2.0, 1.4]
+_DP_CRUISE_MAX_V_FOLLOWING = [3.0, 2.3, 1.4, 0.9, 0.5]
 _DP_CRUISE_MAX_BP = [0., 5., 10., 20., 55.]
 
 # Lookup table for turns
